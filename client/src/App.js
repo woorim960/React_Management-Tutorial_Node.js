ff3079c4bd8f1d6f16ac58ef26cd88928811c77b
박우림 <park-woorim@bag-ulim-ui-MacBook-Pro-7.local>
2020-02-25 00:22:33 +0900

    Error Complet 'Client App.js'

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer'
 import Paper from '@material-ui/core/Paper'
@@ -13,7 +12,7 @@ import { withStyles } from '@material-ui/core/styles'
 const styles = theme => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     overflowX: 'auto'
   },
   table: {
@@ -51,21 +50,21 @@ class App extends Component {
     const { classes } = this.props
     return (
       <Paper className={classes.root}>
-        <Table className={classes.table}> 
-            <Table>
-              <TableHead>
+        <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
                 <TableCell>번호</TableCell>
                 <TableCell>이미지</TableCell>
                 <TableCell>이름</TableCell>
                 <TableCell>생년월일</TableCell>
                 <TableCell>성별</TableCell>
                 <TableCell>직업</TableCell>
-              </TableHead>
-              
-              <TableBody>
-                {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)})}
-              </TableBody>
-            </Table>
+              </TableRow>
+            </TableHead>
+            
+            <TableBody>
+              {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)})}
+            </TableBody>
         </Table>
       </Paper>
     )
