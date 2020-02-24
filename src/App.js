d9149c316e2bb299a9b5977045279f988df27b4f
박우림 <park-woorim@bag-ulim-ui-MacBook-Pro-7.local>
2020-02-24 19:16:36 +0900

    Create customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management!!</h2>
-    </div>
-  );
+const customers = [
+  {
+  'id': 1,
+  'image': 'http://placeimg.com/128/128/1',
+  'name':'박우림',
+  'birthday':'960906',
+  'gender':'남자',
+  'job':'대학생'
+},{
+  'id': 2,
+  'image': 'http://placeimg.com/128/128/2',
+  'name':'홍길동',
+  'birthday':'960906',
+  'gender':'남자',
+  'job':'프로그래머'
+},{
+  'id': 3,
+  'image': 'http://placeimg.com/128/128/3',
+  'name':'박주영',
+  'birthday':'010615',
+  'gender':'여자',
+  'job':'대학생'
+}
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer 
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            )
+          })
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
