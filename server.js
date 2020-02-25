24cd82b36aa5c076773cc17da7121b16e1cbc3d3
박우림 <park-woorim@bag-ulim-ui-MacBook-Pro-7.local>
2020-02-25 17:07:46 +0900

    Create Customer Database Table & Node.js Database Connection Setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
@@ -6,31 +7,24 @@ const port = process.env.PORT || 5000
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+const data = fs.readFileSync('./database.json')
+const conf = JSON.parse(data)
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+})
+connection.connect()
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'http://placeimg.com/128/128/1',
-            'name':'박우림',
-            'birthday':'960906',
-            'gender':'남자',
-            'job':'대학생'
-        },{
-            'id': 2,
-            'image': 'http://placeimg.com/128/128/2',
-            'name':'라이언구하기',
-            'birthday':'960906',
-            'gender':'남자',
-            'job':'대학생'
-        },{
-            'id': 3,
-            'image': 'http://placeimg.com/128/128/3',
-            'name':'박주영',
-            'birthday':'010615',
-            'gender':'여자',
-            'job':'대학생'
+    connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
+        res.send(rows)
         }
-    ])
+    )
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
