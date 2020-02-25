b51e9a7e349f514ffdc42d5f19f1977b63cbc97d
박우림 <park-woorim@bag-ulim-ui-MacBook-Pro-7.local>
2020-02-25 23:44:04 +0900

    Add Customer Search & Finish Project

--- client/src/App.js
+++ client/src/App.js
@@ -99,14 +99,16 @@ class App extends Component {
     super(props)
     this.state = {
       customers: '',
-      completed: 0
+      completed: 0,
+      searchKeyword: ''
     }
   }
 
   stateRefresh = () => {
     this.setState({
       customers: '',
-      completed: 0
+      completed: 0,
+      searchKeyword: ''
     })
     this.callApi()
       .then(res => this.setState({customers: res}))
@@ -130,7 +132,21 @@ class App extends Component {
     const { completed } = this.state
     this.setState({ completed: completed >= 100 ? 0 : completed + 1})
   }
+
+  handleValueChange= (e) => {
+    let nextState = {}
+    nextState[e.target.name] = e.target.value
+    this.setState(nextState)
+  }
   render() {
+    const filteredComponents = (data) => {
+      data = data.filter((c) => {
+        return c.name.indexOf(this.state.searchKeyword) > -1
+      }) 
+      return data.map((c) => {
+        return <Customer stateRefresh={this.stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>
+      })
+    }
     const { classes } = this.props
     const cellList = ["번호", "프로필 이미지", "이름", "생년월일", "성별", "직업", "설정"]
     return (
@@ -154,6 +170,8 @@ class App extends Component {
               }}
               inputProps={{ 'aria-label': 'search' }}
             />
+            name="searchKeyword"
+            value={this.state.searchKeyword}
           </div>
         </Toolbar>
         </AppBar>
@@ -171,9 +189,8 @@ class App extends Component {
               </TableHead>
               
               <TableBody>
-                {this.state.customers ? this.state.customers.map(c => { 
-                  return ( <Customer stateRefresh={this.stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)
-                }) : 
+                {this.state.customers ? 
+                  filteredComponents(this.state.customers) :
                 <TableRow>
                   <TableCell colSpan="6" align="center">
                     <CircularProgress className={classes.progress} variant="daterminate" value={this.state.completed} color="secondary"/>
